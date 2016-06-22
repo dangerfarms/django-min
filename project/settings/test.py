26ff76bf1c26ae1e82bf8bdf52b6bbcc90cf423b
Lewis Taylor <lewis@dangerfarms.com>
2016-06-22 18:54:26 +0100

    Import config package in test settings

--- project/settings/test.py
+++ project/settings/test.py
@@ -1,11 +1,10 @@
 import logging
 from project.settings import *
-from project.secrets import *  # Force reload of just secrets so developer specific secrets don't override
+from project.config import *  # Force reload of just secrets so developer specific secrets don't override
 
 
 logging.disable(logging.CRITICAL)
 
-# Additional Config needed to
 CELERY_ALWAYS_EAGER = True
 EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 PASSWORD_HASHERS = (
